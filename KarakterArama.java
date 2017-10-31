7c7bba0c82bcd068987e5eac18f31e23dabf8d72
omerates760 <33201732+omerates760@users.noreply.github.com>
2017-10-31 10:43:57 +0200

    indexof-trim-replace
    metinde karakter arama ,metnin baş ve sonundaki boşlukları temizleme,string ifadeyi yeni string ifadeyle değiştirme

---
+++ KarakterArama.java
@@ -0,0 +1,17 @@
+package Orta;
+
+public class KarakterArama {
+
+	public static void main(String[] args) {
+		String metin="Metin bir, Arapça'ya mensup bir kelime olup, "
+				+ "mtn köklerinden türemiþ,	 'yazý parçasý, yazýyý oluþturan "
+				+ "unsurlarýn her bir bölümü' olarak tanýmlanmýþtýr";
+		String j="      merhaba dünya     ";
+		String i;
+		//i=metin.indexOf("t");-----------String adresini verir
+		//i=metin.replace("bir", "iki");----Stringleri deðiþtirir
+		//i=j.trim();----------------------Basta ve sondaki bosluklarý kaldirir
+		System.out.println(i);
+	}
+
+}
